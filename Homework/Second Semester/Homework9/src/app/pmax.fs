078287c2177e1e016b768f02760d9416fef6d8ec
Lexalexeev <alexey_alexeev@bk.ru>
2015-05-03 04:10:18 +0300

    Add Tasks 46-47

---
+++ Homework/Second Semester/Homework9/src/app/pmax.fs
@@ -0,0 +1,63 @@
+﻿// Homework 9
+// Alekseev Aleksei, group 171.
+
+module pmax
+
+open System.Threading
+
+// Task 46 
+let maxInRange (arr : int []) l r : int =
+  let mutable res = 0
+  for i in l .. r do
+    if arr.[i] > res then
+      res <- arr.[i]
+  res
+
+let max (arr : int []) (threadNum : int) =
+  let arraySize = arr.Length
+  let res = ref 0
+  let step = arraySize / threadNum   
+  let threadArray = Array.init threadNum (fun i ->
+    new Thread(ThreadStart(fun _ ->
+        let threadRes = maxInRange arr (i * step) ((i + 1) * step - 1)
+        Monitor.Enter(res)
+        try
+          if threadRes > res.Value then res := threadRes
+        finally
+          Monitor.Exit(res)
+        ))
+    )
+  for t in threadArray do
+    t.Start()
+  for t in threadArray do
+    t.Join()
+  res.Value
+
+// Task 47 
+let integralInRange (f : float -> float) l r e =
+  let mutable res = 0.0
+  for i in l .. e .. (r - e) do 
+    res <- res + 0.5 * (f(i) + f(i + e)) * e
+  res
+
+let defIntegral (threadNumber : int) f l r e : float =
+  let res = ref 0.0
+  let step = (r - l) / (float threadNumber)
+  let threadArray = Array.init threadNumber (fun i ->
+      new Thread(ThreadStart(fun _ ->
+          let threadRes =
+            integralInRange f (l + step * (float i)) (l + step * (float (i + 1))) e
+          Monitor.Enter(res)
+          res := res.Value + threadRes
+          Monitor.Exit(res)
+        ))
+    )
+  for t in threadArray do
+    t.Start()
+  for t in threadArray do
+    t.Join()
+  res.Value
+
+[<EntryPoint>]
+let main argv =
+  0
---
+++ Homework/Second Semester/Homework9/src/test/tests.fs
@@ -0,0 +1,41 @@
+﻿// Homework 9
+// Alekseev Aleksei, group 171.
+
+module tests
+
+open NUnit.Framework
+open pmax
+
+[<TestCase (1, Result = 999999)>]
+[<TestCase (2, Result = 999999)>]
+[<TestCase (3, Result = 999999)>]
+[<TestCase (4, Result = 999999)>]
+let ``TestMaxRnd6`` (t : int) =
+  let degree = 6
+  let n = pown 10 degree
+  let rnd = new System.Random(0)
+  let arr = Array.init n (fun i -> rnd.Next(0 , n))
+  max arr t
+
+[<TestCase (1, Result = 99999998)>]
+[<TestCase (2, Result = 99999998)>]
+[<TestCase (3, Result = 99999998)>]
+[<TestCase (4, Result = 99999998)>]
+let ``TestMaxRnd8`` (t : int) =
+  let degree = 8
+  let n = pown 10 degree
+  let rnd = new System.Random(0)
+  let arr = Array.init n (fun i -> rnd.Next(0 , n))
+  max arr t
+
+[<TestCase ([|1;7;3;0;4;5;8;6;2;1;7;3;0;4;5;8;6;2;9;1;7;3;0;4;5;8;6;2|],1, Result = 9)>]
+[<TestCase ([|1;7;3;0;4;5;8;6;2;1;7;3;0;4;5;8;6;2;9;1;7;3;0;4;5;8;6;2|],2, Result = 9)>]
+[<TestCase ([|1;7;3;0;4;5;8;6;2;1;7;3;0;4;5;8;6;2;9;1;7;3;0;4;5;8;6;2|],3, Result = 9)>]
+[<TestCase ([|1;7;3;0;4;5;8;6;2;1;7;3;0;4;5;8;6;2;9;1;7;3;0;4;5;8;6;2|],4, Result = 9)>]
+let ``TestMax`` (arr : int []) (t : int) =
+  max arr t
+
+[<TestCase(1, 0.0, 7.0, 0.5, Result = 38.5)>]
+[<TestCase(2, -6.0, 0.0, 1, Result = -6.0)>]
+let ``TestIntegral`` (threadNumber : int) l r e : float  =
+  defIntegral threadNumber (fun x -> x + 2.0) l r e 
