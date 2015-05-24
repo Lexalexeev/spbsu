9b7af8c728070682f5bb2dffa068221d91fd6e55
Lexalexeev <alexey_alexeev@bk.ru>
2015-05-24 15:38:00 +0300

    Fixed Task 49

--- Homework/Second Semester/Homework10/mergesort/src/app/mergesort.fs
+++ Homework/Second Semester/Homework10/mergesort/src/app/mergesort.fs
@@ -5,31 +5,30 @@ module mergesort
 
 open System.Threading
 
-let rec mergeInRange (l : int []) (r : int []) (res : int [] ref) =
+let rec mergeInRange (l : int []) (r : int []) (res : int []) =
   let n = l.Length + r.Length
   let mutable i = 0
   let mutable j = 0
   for k = 0 to n - 1 do
     if i >= l.Length   then 
-      res.Value.[k] <- r.[j]
+      res.[k] <- r.[j]
       j <- j + 1
     elif j >= r.Length then 
-      res.Value.[k] <- l.[i]
+      res.[k] <- l.[i]
       i <- i + 1
     elif l.[i] < r.[j] then 
-      res.Value.[k] <- l.[i]
+      res.[k] <- l.[i]
       i <- i + 1
     else 
-      res.Value.[k] <- r.[j]
+      res.[k] <- r.[j]
       j <- j + 1
-  res.Value
+  res
  
 let rec sort (arr : int []) (threadNum : int) (res : int [] ref) =
   match arr with
   | [||] -> [||]
   | [|a|] -> [|a|]
-  | arr ->   
-    let res = ref (Array.zeroCreate(arr.Length))
+  | arr -> 
     let n = arr.Length
     if threadNum > 1 then
       let l = ref [||]
@@ -40,9 +39,9 @@ let rec sort (arr : int []) (threadNum : int) (res : int [] ref) =
       rThread.Start()
       r := sort arr.[(n / 2)..(n - 1)] (threadNum / 2) res
       rThread.Join()
-      lock res (fun _ -> mergeInRange l.Value r.Value res)
+      lock res (fun _ -> mergeInRange l.Value r.Value res.Value.[0..(n - 1)])
     else 
-      mergeInRange (sort arr.[0..(n / 2) - 1] 0 res) (sort arr.[(n / 2)..(n - 1)] 0 res) res
+      mergeInRange (sort arr.[0..(n / 2) - 1] 0 res) (sort arr.[(n / 2)..(n - 1)] 0 res) res.Value.[0..(n - 1)]
 
 let mergeSort (arr : int []) (threadNum : int) =
   let res = ref(Array.zeroCreate(arr.Length))
